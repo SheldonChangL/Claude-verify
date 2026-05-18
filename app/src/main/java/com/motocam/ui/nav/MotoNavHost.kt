d10ab65df6d2ae36281dac43b0c687369c682311
SheldonChang <sheldon.chang@jet-opto.com.tw>
2026-05-18 09:23:12 +0800

    feat(alerts): AlertsScreen 1:1 mirror (#25)
    - 新增 AlertsScreen() stateless composable，內含 hard-code 10 筆 mock alerts
    - MotoNavHost alerts route 接入 AlertsScreen()
    - Strings 新增 ALERTS_* / ALERT_TITLE_* 常數
    
    Change-Id: I098a71ba9e75140a0c1ee3bd15327d965f4da1fe

--- app/src/main/java/com/motocam/ui/nav/MotoNavHost.kt
+++ app/src/main/java/com/motocam/ui/nav/MotoNavHost.kt
@@ -5,6 +5,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.motocam.ui.screens.alerts.AlertsScreen
 import com.motocam.ui.screens.home.HomeScreen
 import com.motocam.ui.screens.playback.PlaybackScreen
 
@@ -13,7 +14,7 @@ fun MotoNavHost(navController: NavHostController) {
     NavHost(navController = navController, startDestination = "home") {
         composable("home") { HomeScreen() }
         composable("playback") { PlaybackScreen() }
-        composable("alerts") { Text("alerts") }
+        composable("alerts") { AlertsScreen() }
         composable("settings") { Text("settings") }
     }
 }
